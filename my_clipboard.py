9a071422ddd184296e8a41534b66c182e18345af
tzlmzyt <arthursimpsons@outlook.com>
2022-04-16 23:33:34 +0800

    Added more -help

--- my_clipboard.py
+++ my_clipboard.py
@@ -1,22 +1,22 @@
 import sys
 import clipboard
 import json
-import data
+#import data
 
 
-print("")
-print("--------------------------- 1 ------------------------")
-print(sys)
-print("")
-print(sys.argv)
-print("")
-print(clipboard)
-print("")
+# print("")
+# print("--------------------------- 1 ------------------------")
+# print(sys)
+# print("")
+# print(sys.argv)
+# print("")
+# print(clipboard)
+# print("")
 # print(json)
 # print("") 
 # print("") 
 # print("") 
-print("--------------------------- 2 ------------------------")
+#print("--------------------------- 2 ------------------------")
 
 # print("") 
 # print(sys.argv[1:])
@@ -27,7 +27,7 @@ print("--------------------------- 2 ------------------------")
 # print("") 
 # print("") 
 # print("") 
-print("--------------------------- 3 ------------------------")
+#print("--------------------------- 3 ------------------------")
 
 
 
@@ -39,23 +39,23 @@ print("--------------------------- 3 ------------------------")
 def save_json(filepath, data):
     with open(filepath, "w"):
         # as f
-        f = open(filepath, "w")
+        #f = open(filepath, "w")
         
-        print("* f:", end=" ")
-        print(f)
-        print("")
+        #print("* f:", end=" ")
+        #print(f)
+        #print("")
         
-        print("""* open(filepath, "w"):""", end=" ")
-        print(open(filepath, "w"))
-        print("")
+        #print("""* open(filepath, "w"):""", end=" ")
+        #print(open(filepath, "w"))
+        #print("")
                 
         json.dump(data, open(filepath, "w"), sort_keys=True, indent=4)
         # json.dump(data, f)
-        print("* json.dump:", end=" ")
-        print(json.dump)
+        #print("* json.dump:", end=" ")
+        #print(json.dump)
         
-        print("* json.dump(data, f):", end=" ")
-        print("")
+        #print("* json.dump(data, f):", end=" ")
+        #print("")
 
 
 # save_json("data.json", {'key2':'hhh'})
@@ -65,29 +65,29 @@ def load_json(filepath):
     try:
         with open(filepath, "r"):
             f2 = open(filepath, "r")
-            print("* f2:", end=" ")
-            print(f2)
+            #print("* f2:", end=" ")
+            #print(f2)
 
-            print("""* open(filepath, "r"):""", end=" ")
-            print(open(filepath, "r"))
-            print("")        
-            
-            
+            #print("""* open(filepath, "r"):""", end=" ")
+            #print(open(filepath, "r"))
+            #print("")                             
             data = json.load(f2)
-            print("""* json.load(open(filepath, "r")):""" , end = " ")
-            print(json.load(open(filepath, "r")))
+            #print("""* json.load(open(filepath, "r")):""" , end = " ")
+            #print(json.load(open(filepath, "r")))
             
-            print("* data:", end = "")
-            print(data)
+            #print("* data:", end = "")
+            #print(data)
             return data
     except:    
+        print("There is not a data.json file. You need to save something at first\n")
         return {}
+        
 
 
-print("------------------ 4 ----------------")
+#print("------------------ 4 ----------------")
 
 # load_json(SAVE_DATA)
-load_json("data.json")
+#load_json("data.json")
 
 
 
@@ -122,24 +122,29 @@ print(data.dictions)
 
 
 
-print("") 
-print("------------------------------------ 5 -----------------") 
+# print("") 
+# print("------------------------------------ 5 -----------------") 
 
 if len(sys.argv) > 1:    
     command = sys.argv[1]
+    print("---------------------------------------------------")
     print("The command has got： " + command +"\n")
     data = load_json("data.json")
     if command == "-save":
         # key = input("Enter a key: ")
         key2 = command = sys.argv[2]
+        #command2 = sys.argv[2]
+        #print(command2)
         data[key2] = sys.argv[3]
-        print(sys.argv[3])
+        #command3 = sys.argv[3]
+        #print(command3)
         save_json("data.json", data)
         
         print("I have already saved your text.")
         
     elif command == "-load":
-        key = input("Enter a key: ")
+        #key = input("Enter a key: ")
+        key = sys.argv[2]
         if key in data:
             clipboard.copy(data[key])
             print("I have already copied text. \nKey: " + key + "\nValue: " + data[key])
@@ -147,21 +152,21 @@ if len(sys.argv) > 1:
             print("Key does not exist.")     
         
     elif command == '-list':
+        print("I have already listed your text:\n")
         print(data)
-        print("I have already listed your text.")
+        
     elif command == "-help":
         print("")
-        print("my_clipboard.py -save 1234 4321")
-        print("I will save your text: 4321 in the key: 1234")
+        print("my_clipboard.py -save 12345 54321")
+        print("I will save your text: 54321 in the key: 12345")
         print("") 
+        print("my_clipboard.py -load 12345")
+        print("I will load&copied your text: 54321")
+        print("---------------------------------------------------")
     else:
         print('Aw, It looks like a unkown command.')
 else:
-    print("Tring to use this command: -save, -load, -list, -help.")
-
-
-print("") 
-print("") 
-print("") 
-
-
+    print("---------------------------------------------------")
+    print("Tring to use this command: -help, -save, -load, -list")
+    print("For instance: \nmy_clipboard.py -hlep")
+    print("---------------------------------------------------")
